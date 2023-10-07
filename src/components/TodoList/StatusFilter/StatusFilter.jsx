392b953a609e97d1f9eefe86b61de04b86d752e9
Sokilskill <vprovorotov@gmail.com>
2023-10-07 13:16:32 +0300

    redux

--- src/components/TodoList/StatusFilter/StatusFilter.jsx
+++ src/components/TodoList/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from '../Button/Button';
 import css from './StatusFilter.module.css';
+import { statusFilters } from 'redux/constants';
+import { setStatusFilter } from 'redux/actions';
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
+
+  const filter = useSelector(state => state.filters.status);
+
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        onClick={() => handleFilterChange(statusFilters.all)}
+        selected={filter === statusFilters.All}
+      >
+        All
+      </Button>
+      <Button
+        onClick={() => handleFilterChange(statusFilters.active)}
+        selected={filter === statusFilters.active}
+      >
+        Active
+      </Button>
+      <Button
+        onClick={() => handleFilterChange(statusFilters.completed)}
+        selected={filter === statusFilters.completed}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
